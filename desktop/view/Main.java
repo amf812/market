54d2a7e894a250678d1eb0047593f8770ac82a93
Alex <fountaia@mail.gvsu.edu>
2017-11-20 14:46:53 -0500

    Transfered Project into repo.

---
+++ desktop/view/Main.java
@@ -0,0 +1,41 @@
+package view;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.*;
+
+public class Main extends Application {
+
+    FirebaseOptions options = new FirebaseOptions.Builder()
+            .setCredentials(GoogleCredentials.getApplicationDefault())
+            .setDatabaseUrl("https://postings.firebaseio.com/")
+            .build();
+
+    FirebaseApp.initializeApp(options);
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("Marketplace.fxml"));
+        primaryStage.setTitle("GV Marketplace");
+        primaryStage.setScene(new Scene(root, 850, 700));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+
+
+
+}
+
