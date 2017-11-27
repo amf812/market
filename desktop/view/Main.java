a02848f8caa8300ef66995f9decb41134e65a95e
Alex <fountaia@mail.gvsu.edu>
2017-11-27 14:35:05 -0500

    Moved Create Post Button

--- desktop/view/Main.java
+++ desktop/view/Main.java
@@ -7,6 +7,10 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Main extends Application{
 
@@ -30,6 +34,26 @@ public class Main extends Application{
     public static void main(String[] args) {
         launch(args);
     }
+
+
+    public static String getHTML(String urlToRead) throws Exception {
+        StringBuilder result = new StringBuilder();
+        URL url = new URL(urlToRead);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+        rd.close();
+        return result.toString();
+    }
+
+
+        // Example System.out.println(getHTML(args[0]));
+
+
 
 }
 
