84d94627d003f2956aa985d5d9e6407bff185b42
greenese <31932398+greenese@users.noreply.github.com>
2017-11-26 22:07:55 -0500

    App now opens with login page. Each of the buttons on the login page now take you to a different scene.

--- desktop/view/LoginController.java
+++ desktop/view/LoginController.java
@@ -1,16 +1,34 @@
 package view;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
 public class LoginController {
 
-    public void loginButtonClicked(){
-        System.out.println("Login clicked.");
+    //Changes scenes.
+    public void loginButtonClicked() throws Exception{
+        Parent scene2 = FXMLLoader.load(getClass().getResource("Marketplace.fxml"));
+        Scene sc2 = new Scene(scene2);
+        Main.window.setTitle("GV Marketplace");
+        Main.window.setScene(sc2);
+        Main.window.show();
     }
 
-    public void fogotPasswordButtonClicked(){
-        System.out.println("Forgot Password clicked.");
+    public void forgotPasswordButtonClicked() throws Exception{
+        Parent scene3 = FXMLLoader.load(getClass().getResource("Forgot Password.fxml"));
+        Scene sc3 = new Scene(scene3);
+        Main.window.setTitle("Forgot Password");
+        Main.window.setScene(sc3);
+        Main.window.show();
     }
 
-    public void createAccountButtonClicked(){
-        System.out.println("Create account clicked.");
+    public void createAccountButtonClicked() throws Exception{
+        Parent scene4 = FXMLLoader.load(getClass().getResource("CreateAccount.fxml"));
+        Scene sc4 = new Scene(scene4);
+        Main.window.setTitle("Account Creation");
+        Main.window.setScene(sc4);
+        Main.window.show();
     }
 }
--- desktop/view/Main.java
+++ desktop/view/Main.java
@@ -10,6 +10,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.awt.*;
 import java.io.*;
 
 public class Main extends Application{
@@ -22,13 +24,30 @@ public class Main extends Application{
     FirebaseApp.initializeApp(options);
 
 
+
     @Override*/
+    static Stage window;
+
     public void start(Stage primaryStage) throws Exception{
+        window = primaryStage;
+
+        //Labels
+        Label login = new Label("GV Marketplace Login");
+        Label home = new Label("GV Marketplace");
+
+        //Layouts
+        Parent scene1 = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        window.setTitle("GV Marketplace Login");
+        window.setScene(new Scene(scene1, 850, 600));
+        window.show();
+    }
+
+    /*public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Marketplace.fxml"));
         primaryStage.setTitle("GV Marketplace");
         primaryStage.setScene(new Scene(root, 850, 700));
         primaryStage.show();
-    }
+    }*/
 
 
     public static void main(String[] args) {
